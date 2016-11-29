92c6aec8ab224ada0d918f1fde290d199b833caf
Ryan <ryan.ross@temple.edu>
2016-11-28 20:31:36 -0500

    Merge branch 'master' of https://github.com/shayd3/CIS3309-Labs

---
+++ ProductPOS - Ryan Ross/ProductPOS - Ryan Ross/ProductDB.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductPOS___Ryan_Ross
+{
+    class ProductDB
+    {
+        public static OleDbConnection GetConnection()
+        {
+            string connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;" +
+                "Data Source=|DataDirectory|\\ProductDB.accdb;" +
+                "Persist Security Info=True";
+            OleDbConnection connection = new OleDbConnection(connectionString);
+            return connection;
+        }
+    }
+}
