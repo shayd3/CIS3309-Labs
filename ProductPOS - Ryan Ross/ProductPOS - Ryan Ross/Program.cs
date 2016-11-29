d6e4efe7cac36a31d9cb371c84a48067d1b590fc
Ryan <ryan.ross@temple.edu>
2016-11-28 20:34:45 -0500

    Init

--- ProductPOS - Ryan Ross/ProductPOS - Ryan Ross/Program.cs
+++ ProductPOS - Ryan Ross/ProductPOS - Ryan Ross/Program.cs
@@ -16,7 +16,7 @@ namespace ProductPOS___Ryan_Ross
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmMain());
         }
     }
 }
